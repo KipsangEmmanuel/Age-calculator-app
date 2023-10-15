082ee3c105141d0bdeaac478a2ad6cdde2a6a67c
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-10-15 09:48:53 -0700

    validate day

--- age-calculator-app-main/input.js
+++ age-calculator-app-main/input.js
@@ -14,4 +14,49 @@ let inputDate = {
     year: null
 };
 
-const calButton = document.getElementById('btnt')
+const calButton = document.getElementById('btnt');
+
+function calculateTimePassed(){
+    clearErrors(['month', 'day', 'year']);
+    validateInputDate();
+}
+
+function validateInputDate() {
+    //collect input from HTML form  and convert into date format
+    monthInputValue = +monthInput.value;
+    dayInputValue = +dayInput.value;
+    yearInputValue = +yearInput.value;
+
+    inputDate = {
+        fullDate: new Date(`${yearInputValue}, ${monthInputValue}, ${dayInputValue}`),
+        month: monthInputValue,
+        day: dayInputValue,
+        year: yearInputValue,
+    };
+
+    const {month, day, year} = inputDate;
+
+    //validate input
+    let validMonth = true;
+    let validDay = true;
+    let validYear = true;
+
+    //update month for leap year
+    (!(year % 4) && year % 100) || !(year % 400) ? (daysPerMonth[1] = 29) : (daysPerMonth[1] = 28);
+
+    //validate day of month is within range
+    if(!dayInputValue) {
+        validDay = false;
+        showError('day-input-error', 'day-error', 'This field is required');
+    }else if(
+        day > daysPerMonth[month - 1] ||
+        day > 31 ||
+        day < 1 ||
+        (day > new Date().getDate() && month === new Date().getMonth() + 1 && year === new Date().getFullYear())
+    ){
+        validDay = false;
+        showError('day-input-error', 'day-error', 'Must be a valid day')
+    }
+
+
+}
